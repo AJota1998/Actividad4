730e695baec31f5ab2aea3c5d045f47db136d078
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2022-04-29 02:03:43 +0200

    NUEVA RAMA COMMIT1

---
+++ actividad4/src/actividad4cambio1/clasenueva.java
@@ -0,0 +1,12 @@
+package actividad4cambio1;
+
+public class clasenueva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HOLA");
+
+	}
+
+}
